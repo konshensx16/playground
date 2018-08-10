7e1a263f1d956c3d2de085e00002b62cb45429bc
konshensx16 <konshensx16@gmail.com>
2018-08-10 01:03:26 +0100

    more work on the searchFragment and specifically (temporary solution) the result is a list of listings which is cleared each time the user types a new character

--- app/src/main/java/com/example/konshensx/firstapp/SearchFragment.java
+++ app/src/main/java/com/example/konshensx/firstapp/SearchFragment.java
@@ -13,15 +13,17 @@ import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.Iterator;
+import java.util.ArrayList;
 import java.util.List;
 
 public class SearchFragment extends Fragment implements OnTaskCompleted{
     EditText currency_search_input;
     private List<Listing> listingList;
+    private List<Listing> searchList;
     private String jsonResponse;
     private static final String TAG = "SearchFragment";
     final OnTaskCompleted listener;
@@ -47,6 +49,7 @@ public class SearchFragment extends Fragment implements OnTaskCompleted{
         super.onViewCreated(view, savedInstanceState);
         currency_search_input = view.findViewById(R.id.currency_search_input);
         resultHolderView = view.findViewById(R.id.result_holder);
+        searchList = new ArrayList<>();
         // TODO: this code needs to move to somewhere like 'onViewCreated'
         // Where the code needs to be
         // TODO: might need to check if the length != 0 (optimization)
@@ -65,17 +68,26 @@ public class SearchFragment extends Fragment implements OnTaskCompleted{
 
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                Log.i(TAG, "onTextChanged: Text changed to: " + charSequence);
+                // clear the list off the previous items
+                searchList.clear();
                 // TODO: search within the list for the user input 'currency in question'
-                // probably just do a linear search for now and then improve it later
+                // probably just do a linear search for now and then improve it later, which will only get one result atm
                 try {
                     for (int index = 0; index < listingList.size(); index++) {
                         Listing listingObject = listingList.get(index);
-                        if (listingObject.getName() == charSequence) {
+                        // checking if the name contains the typed character (user input)
+                        // probably make the user input lower case too, for consistency and yielding better result
+                        if (listingObject.getName().toLowerCase().contains(charSequence)) {
                             // XXX; stop the search and show the result, currently showing just the first result
-                            // TODO: i'm going to need to create an adapter and recyclerView for the search result
+                            // TODO: i'm gonna need to create an adapter and recyclerView for the search result
+                            // add the item (the one found) to the list
+                            searchList.add(listingObject);
+                            // TODO: remove this line, just for testing purposes
                             resultHolderView.setText(listingObject.getName());
                         }
                     }
+                    Log.i(TAG, "onTextChanged: Just to debug the searchList");
                 } catch (Exception e)
                 {
                     e.printStackTrace();
@@ -101,6 +113,7 @@ public class SearchFragment extends Fragment implements OnTaskCompleted{
             Log.i(TAG, "onTaskCompleted: Response code from the API: " + statusCode);
             if (result != null) {
                 this.jsonResponse = result;
+                listingList = new ArrayList<>();
                 // XXX: set the jsonResponse string to the text
                 resultHolderView.setText(result);
             } else {
@@ -115,33 +128,37 @@ public class SearchFragment extends Fragment implements OnTaskCompleted{
         try {
             // TODO: create custom exceptions and check if the values are not null
             JSONObject jsonObject = new JSONObject(this.jsonResponse);
-            JSONObject dataObject = jsonObject.getJSONObject("data");
+            // The listing is actually an array of JsonObjects and not an object of objects, using an array is better than an iterator, i guess
+            // TODO: JSONException @5098 thrown here
+            JSONArray dataArray = jsonObject.getJSONArray("data");
 
             // iterate over the data objects
-            Iterator<String> keys = dataObject.keys();
-            while (keys.hasNext()) {
+
+            for (int index = 0; index < dataArray.length(); index++)
+            {
+                JSONObject dataArrayObject = dataArray.getJSONObject(index);
                 int id = 0;
                 // check if values are actually defined
-                if (dataObject.isNull("id")) {
-                    id = dataObject.getInt("id");
+                if (!(dataArrayObject.isNull("id"))) {
+                    id = dataArrayObject.getInt("id");
                 }
 
                 String name = "No name";
                 // check if values are actually defined
-                if (dataObject.isNull("name")) {
-                    name = dataObject.getString("name");
+                if (!(dataArrayObject.isNull("name"))) {
+                    name = dataArrayObject.getString("name");
                 }
 
                 String symbol = "No symbol";
                 // check if values are actually defined
-                if (dataObject.isNull("symbol")) {
-                    symbol = dataObject.getString("symbol");
+                if (!(dataArrayObject.isNull("symbol"))) {
+                    symbol = dataArrayObject.getString("symbol");
                 }
 
                 String website_slug = "No website_slug";
                 // check if values are actually defined
-                if (dataObject.isNull("website_slug")) {
-                    website_slug = dataObject.getString("website_slug");
+                if (!(dataArrayObject.isNull("website_slug"))) {
+                    website_slug = dataArrayObject.getString("website_slug");
                 }
 
                 Listing listing = new Listing(id, name, symbol, website_slug);
